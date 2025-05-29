43fc5ffd93bcf97cf504d5b5ed4883e53bb9db2d
Sameer Mohammed <personalsameer07@gmail.com>
2025-05-29 21:32:23 +0530

    Remove unused import from main.dart to streamline the codebase.

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:provider/provider.dart';
 import 'package:todoey_flutter/models/task_data.dart';
 import 'package:todoey_flutter/screens/task_screen.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
